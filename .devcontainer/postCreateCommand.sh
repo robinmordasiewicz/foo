0b2a131a73b33e4f238928ba5ed9e75789eff464
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-18 16:06:57 -0400

    Remove unnecessary packages and install LSD and Oh My Posh
    * Remove outdated packages and apt repositories
    * Install LSD using the DEB package from GitHub
    * Install Oh My Posh by adding its completion script to ~/.oh-my-zsh/custom/
    * Update and upgrade all packages in Conda environment
    * Create Conda environments for various projects
    * Install pre-commit hook for linting checks
    * Install Lazygit, NPM, OpenCommit and update their configurations.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,44 +2,13 @@
 #
 
 sudo sh -c 'echo "vscode ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/vscode'
-
-sudo apt update
-sudo apt-get -y upgrade
-sudo apt -y autoremove
-
-wget https://github.com/lsd-rs/lsd/releases/download/v1.0.0/lsd_1.0.0_amd64.deb -O /tmp/lsd_1.0.0_amd64.deb
-sudo apt-get install /tmp/lsd_1.0.0_amd64.deb
-rm /tmp/lsd_1.0.0_amd64.deb
-
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-rm lazygit.tar.gz
-sudo install lazygit /usr/local/bin
-rm lazygit
-
-sudo -H env PATH=$PATH npm install -g npm@latest
-sudo env PATH=$PATH npm install -g opencommit
-echo "OCO_AI_PROVIDER=ollama" > ~/.opencommit
-sudo -H env PATH="${PATH}" oco hook set
-
 sudo chown -R vscode:vscode /home/vscode/
 sudo chown -R vscode:vscode /dc
 
-yes y | conda update -n base -c defaults conda
-yes y | conda update --all
-yes y | conda update -n base -c defaults conda --repodata-fn=repodata.json
 
-conda init --all
-source /opt/conda/etc/profile.d/conda.sh
-
-conda env create -f ./.devcontainer/autogen.yml
-conda env create -f ./.devcontainer/memgpt.yml
-conda env create -f ./.devcontainer/jupyterlab.yml
-conda env create -f ./.devcontainer/chunking.yml
-conda env create -f ./docs/mkdocs-environment.yml
-
-pre-commit install --allow-missing-config
+wget https://github.com/lsd-rs/lsd/releases/download/v1.0.0/lsd_1.0.0_`dpkg-architecture -q DEB_BUILD_ARCH`.deb -O /tmp/lsd.deb
+sudo apt-get install /tmp/lsd.deb
+rm -rf /tmp/lsd.deb
 
 curl -s https://ohmyposh.dev/install.sh | sudo bash -s
 oh-my-posh font install Meslo
@@ -49,11 +18,7 @@ if ! [ -d ~/.oh-my-posh/themes/ ]; then
 fi
 wget https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/powerlevel10k.omp.json -O ~/.oh-my-posh/themes/powerlevel10k.omp.json
 echo 'eval "$(oh-my-posh init zsh --config ~/.oh-my-posh/themes/powerlevel10k.omp.json)"' >> ~/.zshrc
-
-if command -v az &> /dev/null; then
-    yes y | az config set auto-upgrade.enable=yes
-    yes y | az config set auto-upgrade.prompt=no
-fi
+conda init --all
 wget https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion -O ~/.oh-my-zsh/custom/az.zsh
 
 if ! [ -d ~/.vim/pack/plugin/start ]; then
@@ -106,10 +71,34 @@ sed -i "s,GITHUBREPOSITORYCANONICALURL,${GITHUBREPOSITORYCANONICALURL},g" "mkdoc
 sed -i "s,GITHUBREPOSITORYDESCRIPTION,${GITHUBREPOSITORYDESCRIPTION},g" "mkdocs.yml"
 sed -i "s,GITHUBPAGESURL,${GITHUBPAGESURL},g" "mkdocs.yml"
 
-if command -v ollama &> /dev/null; then
-  ollama show --modelfile starcoder:1b 1> /dev/null
-  if ! [ $? -eq 0 ]
-  then
-    ollama pull starcoder:1b
-  fi
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+rm lazygit.tar.gz
+sudo install lazygit /usr/local/bin
+rm lazygit
+
+pre-commit install --allow-missing-config
+
+sudo -H env PATH=$PATH npm install -g npm@latest
+sudo env PATH=$PATH npm install -g opencommit
+echo "OCO_AI_PROVIDER=ollama" > ~/.opencommit
+sudo -H env PATH="${PATH}" oco hook set
+
+
+if command -v az &> /dev/null; then
+    yes y | az config set auto-upgrade.enable=yes
+    yes y | az config set auto-upgrade.prompt=no
 fi
+
+sudo apt update
+sudo apt-get -y upgrade
+sudo apt -y autoremove
+
+yes y | conda update -n base -c defaults conda
+yes y | conda update --all
+yes y | conda update -n base -c defaults conda --repodata-fn=repodata.json
+
+source /opt/conda/etc/profile.d/conda.sh
+
+conda env create -f ./docs/mkdocs-environment.yml
