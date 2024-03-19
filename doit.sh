6652f2b167217c5989bb6e6b76caa7f680649b4c
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-03-19 12:53:47 -0400

    add doit.sh

---
+++ doit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+git remote add -f devcontainer https://github.com/robinmordasiewicz/devcontainer.git
+
+git merge -s ours --no-commit --allow-unrelated-histories devcontainer/main
+
+git read-tree --prefix=.devcontainer/ -u devcontainer/main:.devcontainer
+
+exit
+
+git fetch devcontainer
+git checkout -b devcontainer devcontainer/main
+git checkout main
+git read-tree --prefix=.devcontainer -u devcontainer:.devcontainer
+
+git subtree pull --prefix .devcontainer https://github.com/robinmordasiewicz/devcontainer.git main
